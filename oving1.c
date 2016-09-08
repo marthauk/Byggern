b7302d3eebaae7da352948694c843074116959a3
unknown <marthauk@itk-d656.win.ntnu.no>
2016-09-08 16:09:05 +0200

    Oving 2

--- oving1.c
+++ oving1.c
@@ -9,29 +9,68 @@
 #include <stdlib.h>
 #include <avr/io.h>
 
+//#define F_CPU 1000000UL  //for <util/delay.h>
+#include <util/delay.h> //for _delay_ms()
+
 #include "uart.h"
+#include "SRAM_test.h"
+
 
 
 #define BAUD 9600
 #define FOSC 4915200
 #define MYUBRR FOSC/16/BAUD-1
 
+
 int main(void)
 {	
+	//ov1////////////////////////////////
+	
 	UART_Init(MYUBRR);
 	//DDRD=0x01;
 	//PORTD=0x01;
 	//UART_Transmit(10);
-    while(1)
-    {
-	//	PORTD ^= (1 << PD0);
-	//		for (int i=0;i<1000;i++){
-	//		}
-		int test = 0;
-		printf("hei, %i", test);
-		//unsigned char c=UART_Receive();
-		//UART_Transmit('a');
+	
+	 //while(1){
+		 //	PORTD ^= (1 << PD0);
+		 //for (int i=0;i<1000;i++){
+		 //}
+		 //int test = 0;
+		 //printf("hei, %i", test);
+		 //unsigned char c=UART_Receive();
+		 //UART_Transmit(c);
+	 //}
+	
+	
+	//ov2////////////////////////////////
+	
+	DDRE=0x3;
+	DDRA=0xFF;
+	PORTA=0xFF;
+	PORTE=0x2;
+
+	
+	
+	
 			
-    }
+    //while(1){
+		////for (int i=0;i<1000;i++){
+		////}
+		//_delay_ms(1000);
+		//PORTE|=(1<<PE1);
+		//PORTA^=(1<<PA0);
+		//PORTA^=(1<<PA1);
+		//
+    //}
+	
+	
+	
+	//PORTE|=(1<<PE1);
+	PORTA|=(1<<PA0);
+	PORTA|=(1<<PA1);
+	SRAM_test();
+	
+	
+	
 	return 0;
 }
